f8eb092f8f5a480009f9802e8ab5ce2116235686
chenzhiyuan <791845283@qq.com>
2018-06-25 15:54:27 +0800

    config

---
+++ Application/Admin/Conf/config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    /* 主题设置 */
+    'DEFAULT_THEME' =>  'Default',  // 默认模板主题名称
+    'TMPL_LOAD_DEFAULTTHEME' => true ,
+
+    //'配置项'=>'配置值'
+    'SESSION_PREFIX' => 'GYCrm_' ,
+
+
+    'TMPL_ACTION_SUCCESS' => 'Public/dispatch_jump' ,
+    'TMPL_ACTION_ERROR' => 'Public/dispatch_jump',
+);

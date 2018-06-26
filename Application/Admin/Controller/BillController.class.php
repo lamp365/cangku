5bde3e3dbb1f17a23cf591e3e4bc892fa365d836
kevin.liu <791845283@qq.com>
2018-06-26 11:20:10 +0800

    账单静态页面

---
+++ Application/Admin/Controller/BillController.class.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Admin\Controller;
+
+
+class BillController extends AdminController
+{
+    protected function _initialize(){
+        parent::_initialize();
+    }
+    public function index(){
+
+        $this->display();
+    }
+
+    public function torecord(){
+        $this->display();
+    }
+
+    public function record(){
+        $this->display();
+    }
+
+
+
+
+}

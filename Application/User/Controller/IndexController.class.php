4c2c63ed722fdbe409967e84c29a5e83798c7aa6
kevin.liu <79145283@qq.com>
2018-07-23 09:27:15 +0800

    delete all

--- Application/User/Controller/IndexController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace User\Controller;
-use Think\Controller;
-class IndexController extends Controller {
-    public function index(){
-		echo 'User';
-	}
-}
--- ThinkPHP/Library/Huge/Test.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Huge;
-class Test {
-    public function sayHello(){
-        echo 'hello';
-    }
-}

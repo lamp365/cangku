ce2f8dc1522c2c1741410f6fd8344bb278d9b6b0
kevin.liu <79145283@qq.com>
2018-07-24 10:58:17 +0800

    about

---
+++ Application/Home/Controller/AboutController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+
+class AboutController extends Controller {
+    public function index(){
+        die('稍等！！！');
+        $this->display();
+	}
+
+}

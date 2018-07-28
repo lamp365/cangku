837861433b5c02ee91c3c45b22a958c198612682
kevin.liu <791845283@qq.com>
2018-07-28 15:52:32 +0800

    news

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,6 +6,8 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
         $sets = M('siteconfig')->find();
+        $newData = M('news')->where("tuijian=1")->limit(4)->select();
+        $this->assign("newData",$newData);
         $this->assign("sets",$sets);
         $this->display();
 	}
